1c5712f3a69922f7b7e15b90df6a00d880e75c36
Sterling Evans <s.evans723@gmail.com>
2025-04-22 22:44:02 -0400

    add cancel button with logic to edit state

--- src/features/TodoList/TodoListItem.jsx
+++ src/features/TodoList/TodoListItem.jsx
@@ -1,22 +1,40 @@
 import { useState } from 'react';
+import TextInputWithLabel from '../../assets/shared/TextInputWithLabel';
 
 function TodoListItem({ item, onCompleteTodo }) {
   const [isCompleted, setIsCompleted] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+  const [workingTitle, setWorkingTitle] = useState(item.title);
 
   const handleCheckboxCheck = (itemId) => {
     onCompleteTodo(itemId);
   };
+
+  const handleCancel = () => {
+    setWorkingTitle(item.title);
+    setIsEditing(false);
+  };
   return (
     <li style={{ display: item.isCompleted ? 'none' : 'block' }}>
       <form action="">
-        <input
-          type="checkbox"
-          checked={item.isCompleted}
-          id={item.id}
-          onChange={() => handleCheckboxCheck(item.id)}
-        />
-
-        <span>{item.title}</span>
+        {isEditing ? (
+          <>
+            <TextInputWithLabel value={item.title} />
+            <button onClick={handleCancel}>Cancel</button>
+          </>
+        ) : (
+          <>
+            <label>
+              <input
+                type="checkbox"
+                checked={item.isCompleted}
+                id={item.id}
+                onChange={() => handleCheckboxCheck(item.id)}
+              />
+            </label>
+            <span onClick={() => setIsEditing(true)}>{item.title}</span>
+          </>
+        )}
       </form>
     </li>
   );
